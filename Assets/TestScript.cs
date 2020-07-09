4819da9f2316aba3c7fe1d3ece9309b6df62e000
MarcWerk <47601591+DrOffensive@users.noreply.github.com>
2020-07-10 00:25:26 +0200

    Initial Project commit

---
+++ Assets/TestScript.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TestScript : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+
+        Debug.Log("this is a test");
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
