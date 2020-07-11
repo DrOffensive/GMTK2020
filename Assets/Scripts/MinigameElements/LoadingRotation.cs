1f0c54b3c86e6708cfb5199da219bd6d1de8f8c7
FractialCopper <35343054+FractialCopper@users.noreply.github.com>
2020-07-11 17:37:45 +0200

    Added Miningame

---
+++ Assets/Scripts/MinigameElements/LoadingRotation.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LoadingRotation : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public float ammount = 180f;
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Rotate(new Vector3(0,0,ammount * Time.deltaTime));
+    }
+}
---
+++ Assets/Scripts/Popups/WaitingMinigamePopup.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WaitingMinigamePopup : Minigame
+{
+    public float min_waiting_time = 2f;
+    public float max_waiting_time = 10f;
+    public float close_button_show_time = 3f;
+
+    public GameObject loader;
+    public GameObject close_button;
+
+    
+    
+    
+
+    void Start()
+    {
+        
+        PopupManager.AssignPopup(popup);
+        Cycle();
+    }
+    
+    
+
+
+    public void Cycle() {
+        if (close_button.activeInHierarchy)
+        {
+            StartCoroutine(ShowHideCloseButton(close_button_show_time));
+        }
+
+        else
+            StartCoroutine(ShowHideCloseButton(Random.Range(min_waiting_time, max_waiting_time)));
+    }
+
+
+    public void OverrideSettings(float min_waiting_time,float max_waiting_time,float close_button_show_time)
+    {
+        this.min_waiting_time = min_waiting_time;
+        this.max_waiting_time = max_waiting_time;
+        this.close_button_show_time = close_button_show_time;
+    }
+
+    public void OnSolve()
+    {
+        PopupManager.ClosePopup(popup);
+    }
+
+    
+
+    public IEnumerator ShowHideCloseButton(float time) {
+        yield return new WaitForSeconds(time);
+        close_button.SetActive(!close_button.activeInHierarchy);
+        loader.SetActive(!loader.activeInHierarchy);
+        Cycle();
+    }
+
+}
