cc2f8e3cafef2a9081cacb339a267182a28b40d3
FractialCopper <35343054+FractialCopper@users.noreply.github.com>
2020-07-11 19:38:56 +0200

    Added spot the difference

---
+++ Assets/Scripts/Popups/SpotTheDifferencePopup.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SpotTheDifferencePopup : Minigame
+{
+
+    public Sprite sprite_spot;
+    public Sprite sprite_difference;    
+    public Image image_unchanged;
+    public Image image_changed;
+    public Button button_difference;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        PopupManager.AssignPopup(popup);
+        popup.HideCloseButton = true;
+        image_changed.sprite = image_changed.sprite = sprite_spot;
+        button_difference.image.sprite = sprite_difference;
+        button_difference.transform.gameObject.GetComponent<RectTransform>().localPosition = (Vector3)new Vector2( Random.Range(-60,60), Random.Range(-60, 60));
+    }
+
+    
+    
+
+    public void OnSolve()
+    {
+        PopupManager.ClosePopup(popup);
+
+    }
+}
