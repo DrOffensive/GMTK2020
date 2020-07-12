7502c7dba3edd1f291e1e0d5e0834a85d170d10d
MarcWerk <47601591+DrOffensive@users.noreply.github.com>
2020-07-12 02:50:23 +0200

    Focussing on popups

---
+++ Assets/Scripts/FocusPopup.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class FocusPopup : MonoBehaviour, IPointerClickHandler
+{
+    [SerializeField] int numberOfParents = 1;
+
+    public void OnPointerClick(PointerEventData eventData)
+    {
+        Transform parent = transform.parent;
+        if(numberOfParents > 1)
+        {
+            for (int i = 1; i <= numberOfParents; i++)
+            {
+                Transform p = parent.parent;
+                parent = p;
+            }
+        }
+        if(eventData.button == PointerEventData.InputButton.Left)
+            PopupCanvas.instance.Focus(parent);
+    }
+}
--- Assets/Scripts/PopupCanvas.cs
+++ Assets/Scripts/PopupCanvas.cs
@@ -10,12 +10,21 @@ public class PopupCanvas : MonoBehaviour
     static Canvas canvas;
     static RectTransform rectTransform;
 
+    static PopupCanvas self;
+    public static PopupCanvas instance => self;
+
     public static Canvas Canvas => canvas;
     public static RectTransform RectTransform => rectTransform;
 
     private void Awake()
     {
+        self = this;
         rectTransform = transform as RectTransform;
         canvas = this.GetComponent<Canvas>();
+    }
+
+    public void Focus (Transform popup)
+    {
+        popup.SetAsLastSibling();
     }
 }
