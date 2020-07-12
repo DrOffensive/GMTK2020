d56f7ab4078b90fd5f9951dc7dfbf0eede8182bf
FractialCopper <35343054+FractialCopper@users.noreply.github.com>
2020-07-12 03:04:05 +0200

    Write The Answer

---
+++ Assets/Scripts/Popups/WriteTheAnswerPopup.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class WriteTheAnswerPopup : Minigame
+{
+    string question = "";
+    string answer = "placeholder";
+
+    public TextMeshProUGUI question_text;
+    public TextMeshProUGUI wrong_answer;
+    public TMP_InputField input;
+
+    public Button submit;
+
+
+    
+
+    public int error_counter = 3;
+
+    // Start is called before the first frame update
+    void Start()
+    {       
+        PopupManager.AssignPopup(popup);
+        if (!question.Equals(""))
+            question_text.text = question;
+        error_counter = 3;   
+    }
+
+    public void OnInputChange() {
+        if (input.text.Equals(""))
+            submit.interactable = false;
+        else
+            submit.interactable = true;
+    }
+
+
+    public void OnSubmit() {
+        if (input.text.Equals(answer))
+            OnSolution();
+        else
+            DecreaseCounter();
+    }
+
+    public void DecreaseCounter()
+    {
+        if (!wrong_answer.gameObject.activeInHierarchy) { wrong_answer.gameObject.SetActive(true); }
+        error_counter--;
+        if (error_counter <= 0)
+            OnSolution();
+    }
+
+    public void OnSolution()
+    {
+        PopupManager.ClosePopup(popup);
+    }
+
+   
+}
